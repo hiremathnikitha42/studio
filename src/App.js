54f0c4332622609daa8a46cfccc909605faf9022
hiremathnikitha42 <hiremathnikitha42@gmail.com>
2019-09-13 17:37:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className="studio-container">
+        <div className="studio-image-box">
+        </div>
+        <div className="studio-details-container">
+          <div className="studio-tattoo-title">
+            TIGER, GIRL FACE, TIGER SKIN
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App; 
